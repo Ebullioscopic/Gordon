7e6efedd97c897728a1c7fc881731844bd875e63
Ebullioscopic <hrhn.mudaliar251@gmail.com>
2024-10-15 19:03:31 +0530

    Initial Commit

---
+++ Gordon/GordonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GordonApp.swift
+//  Gordon
+//
+//  Created by admin63 on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GordonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
